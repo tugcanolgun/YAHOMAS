85ebebc82df447716f350db5bee4aad724bce105
Tugcan Olgun <tolgun@live.com>
2018-12-15 18:16:09 +0300

    Add restful api, Add room, booking, items
    Restful api is implemented on /api
    
    Rooms can be fetched
    Today's bookings can be fetched
    Items can be added to bookings and deleted

---
+++ easy/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from easy.models import User, Rooms, Booking, RoomService, RoomServiceBooking
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'url', 'username', 'email', 'groups')
+
+class RoomsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rooms
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    room = RoomsSerializer(read_only=True)
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class ItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomService
+        fields = ('id', 'name', 'price', 'image')
+
+class RoomServiceBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomServiceBooking
+        fields = '__all__'
